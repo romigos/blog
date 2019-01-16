a1ed2da46b2da52672b958b8d77f68c08d191063
romigos <bolist@mail.ru>
2019-01-16 17:45:50 +0200

    Make views to controller Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    post_params.require(:post).permit(:title, :summary, :body)
+  end
 end
