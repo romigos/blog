2660a09ee93dc592148f708f75eaf3ecfc273d95
romigos <bolist@mail.ru>
2019-01-17 22:56:09 +0200

    make views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,16 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
   end
+
   def new
     @post = Post.new
   end
-  def create
+
+    def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to @post
@@ -17,9 +20,23 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(post_params)
+      redirect_to @post
+    else
+      render :edit
+
+    end
+  end
+
   private
 
   def post_params
-    post_params.require(:post).permit(:title, :summary, :body)
+    params.require(:post).permit(:title, :summary, :body)
   end
 end
