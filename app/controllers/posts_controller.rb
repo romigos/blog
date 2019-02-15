24f9b12cee1b35c9714a5d136f20996d820c8173
romigos <bolist@mail.ru>
2019-02-15 18:05:24 +0200

    Make tags for post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.paginate(:page => params[:page], :per_page => 2)
   end
 
   def new
@@ -42,6 +42,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :summary, :body, :image)
+    params.require(:post).permit(:title, :summary, :body, :image, :all_tags)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,15 @@
 
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
+  has_many :taggings
+  has_many :tags, through: :taggings
   validates :title, :summary, :body, presence: true
+
+  def all_tags
+    self.tags.map(&:name).join(' ')
+  end
+  def all_tags=(names)
+    self.tags = names.split(' ').map |name|
+        Tag.where(name: name.strip).first_or_create
+  end
 end
